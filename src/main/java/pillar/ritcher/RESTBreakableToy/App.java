08c07ee57db35b9b872244eec5e4f061471a3ab5
dritcher <dritcher@pillartechnology.com>
2017-03-14 13:47:12 -0400

    first commit

---
+++ src/main/java/pillar/ritcher/RESTBreakableToy/App.java
@@ -0,0 +1,12 @@
+package pillar.ritcher.RESTBreakableToy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
